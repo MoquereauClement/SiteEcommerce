a359e7465778a4f1bc23291cd83d7041bb469ba3
MoquereauClement <112683973+MoquereauClement@users.noreply.github.com>
2024-12-29 14:15:56 +0100

    Revert "Initial commit"
    This reverts commit 6a43b3bc22592933cf5d468001199ac56f001848.

--- JS/pageConnexion.js
+++
@@ -1,4 +0,0 @@
-const params = new URLSearchParams(window.location.search);
-if (params.has('error') && params.get('error') == '1') {
-    document.getElementById('erreur-message').style.display = 'contents';
-}

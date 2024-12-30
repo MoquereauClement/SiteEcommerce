e5f99d006f519b51b5e895bf69a90b21314435be
Aya2712 <aya.boujghouad00@gmail.com>
2024-12-30 15:44:03 +0100

    Update update_product.php

--- update_product.php
+++ update_product.php
@@ -95,7 +95,7 @@ if (isset($_POST['update'])) {
     <!--font awesome cdn link -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <!--custom css file link-->
-    <link rel="stylesheet" href="../css/admin_style.css">
+    
 </head>
 
 <style>
